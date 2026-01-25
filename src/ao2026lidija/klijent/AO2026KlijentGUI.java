786ecd575a2c3e13cb32db3e2248e09178378065
lidija <sestovic.lidija4@gmail.com>
2026-01-25 12:16:02 +0100

    Dodata registracija i baza za cuvanje korisnika

--- src/ao2026lidija/klijent/AO2026KlijentGUI.java
+++ src/ao2026lidija/klijent/AO2026KlijentGUI.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.CardLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
@@ -34,6 +35,23 @@ public class AO2026KlijentGUI extends JFrame {
 	private JButton btnNewButton;
 	private JButton btnPregled;
 	private JButton btnOdjava;
+	private JPanel registracijaPanel;
+	private JLabel lblNewLabel_3;
+	private JButton btnRegistrujPanel;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
+	private JLabel lblNewLabel_7;
+	private JLabel lblNewLabel_8;
+	private JLabel lblNewLabel_9;
+	private JLabel lblNewLabel_10;
+	private JTextField txtIme;
+	private JTextField txtPrezime;
+	private JTextField txtEmail;
+	private JTextField txtKorisnickoImeReg;
+	private JPasswordField txtLozinkaReg;
+	private JButton btnNazadNaPrijavu;
+	private JButton btnRegistracija;
 
 	/**
 	 * Launch the application.
@@ -59,23 +77,27 @@ public class AO2026KlijentGUI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+////////////////////////////////////////
 		setContentPane(contentPane);
 		contentPane.setLayout(new CardLayout(0, 0));
-		contentPane.add(getLoginPanel(), "name_62140405943864");
-		contentPane.add(getMenuPanel(), "name_63004403213294");
+		contentPane.add(getLoginPanel(), "PRIJAVA");
+		contentPane.add(getMenuPanel(), "MENI");
+		contentPane.add(getRegistracijaPanel(), "REGISTRACIJA");
+		////////////////////////////
 	}
 
 	private JPanel getLoginPanel() {
 		if (loginPanel == null) {
 			loginPanel = new JPanel();
 			loginPanel.setLayout(null);
-			loginPanel.add(getLblNewLabel());
 			loginPanel.add(getLblNewLabel_1());
 			loginPanel.add(getLblNewLabel_2());
 			loginPanel.add(getTxtKorisnickoIme());
+			loginPanel.add(getLblNewLabel());
 			loginPanel.add(getTxtLozinka());
 			loginPanel.add(getBtnPrijava());
+			loginPanel.add(getLblNewLabel_3());
+			loginPanel.add(getBtnRegistrujPanel());
 		}
 		return loginPanel;
 	}
@@ -83,7 +105,7 @@ public class AO2026KlijentGUI extends JFrame {
 		if (lblNewLabel == null) {
 			lblNewLabel = new JLabel("Prijava");
 			lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
-			lblNewLabel.setBounds(5, 6, 61, 16);
+			lblNewLabel.setBounds(50, 6, 61, 16);
 		}
 		return lblNewLabel;
 	}
@@ -91,7 +113,7 @@ public class AO2026KlijentGUI extends JFrame {
 		if (lblNewLabel_1 == null) {
 			lblNewLabel_1 = new JLabel("Korisnicko ime:");
 			lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-			lblNewLabel_1.setBounds(5, 35, 109, 16);
+			lblNewLabel_1.setBounds(19, 35, 109, 16);
 		}
 		return lblNewLabel_1;
 	}
@@ -99,7 +121,7 @@ public class AO2026KlijentGUI extends JFrame {
 		if (lblNewLabel_2 == null) {
 			lblNewLabel_2 = new JLabel("Lozinka:");
 			lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-			lblNewLabel_2.setBounds(5, 66, 61, 16);
+			lblNewLabel_2.setBounds(19, 66, 61, 16);
 		}
 		return lblNewLabel_2;
 	}
@@ -107,7 +129,7 @@ public class AO2026KlijentGUI extends JFrame {
 		if (txtKorisnickoIme == null) {
 			txtKorisnickoIme = new JTextField();
 			txtKorisnickoIme.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-			txtKorisnickoIme.setBounds(113, 30, 130, 26);
+			txtKorisnickoIme.setBounds(140, 30, 130, 26);
 			txtKorisnickoIme.setColumns(10);
 		}
 		return txtKorisnickoIme;
@@ -116,14 +138,18 @@ public class AO2026KlijentGUI extends JFrame {
 		if (txtLozinka == null) {
 			txtLozinka = new JPasswordField();
 			txtLozinka.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-			txtLozinka.setBounds(113, 61, 130, 26);
+			txtLozinka.setBounds(140, 61, 130, 26);
 		}
 		return txtLozinka;
 	}
+//////////////////////////////////////////
+	//prijava
+	//////////////////////////////////
+	/////////////////////////////////
 	
 	private JButton getBtnPrijava() {
 	    if (btnPrijava == null) {
-	        btnPrijava = new JButton("Prijavi se");
+	        btnPrijava = new JButton("Prijavite se");
 	        btnPrijava.addActionListener(new ActionListener() {
 	            public void actionPerformed(ActionEvent e) {
 	                // action performed za prijavu!!!!!!!
@@ -145,7 +171,7 @@ public class AO2026KlijentGUI extends JFrame {
 	                            new DataOutputStream(klijentSoket.getOutputStream());
 	                    BufferedReader ulaz =
 	                            new BufferedReader(new InputStreamReader(klijentSoket.getInputStream()));
-
+///////////// LOGIN|username|password\n
 	                    String poruka = "LOGIN|" + username + "|" + password + "\n";
 	                    izlaz.writeBytes(poruka);
 
@@ -168,8 +194,8 @@ public class AO2026KlijentGUI extends JFrame {
 	            }
 	        }); 
 
-	        btnPrijava.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-	        btnPrijava.setBounds(113, 99, 130, 29);
+	        btnPrijava.setFont(new Font("Times New Roman", Font.BOLD, 15));
+	        btnPrijava.setBounds(140, 99, 130, 29);
 	    }
 	    return btnPrijava;
 	}
@@ -214,7 +240,8 @@ public class AO2026KlijentGUI extends JFrame {
 			btnOdjava = new JButton("Odjavi se");
 			btnOdjava.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					((java.awt.CardLayout) contentPane.getLayout()).previous(contentPane);
+					((java.awt.CardLayout) contentPane.getLayout()).show(contentPane, "PRIJAVA");
+;
 
 				}
 			});
@@ -223,4 +250,220 @@ public class AO2026KlijentGUI extends JFrame {
 		}
 		return btnOdjava;
 	}
+	private JPanel getRegistracijaPanel() {
+		if (registracijaPanel == null) {
+			registracijaPanel = new JPanel();
+			registracijaPanel.setLayout(null);
+			registracijaPanel.add(getLblNewLabel_4());
+			registracijaPanel.add(getLblNewLabel_5());
+			registracijaPanel.add(getLblNewLabel_6());
+			registracijaPanel.add(getLblNewLabel_7());
+			registracijaPanel.add(getLblNewLabel_8());
+			registracijaPanel.add(getLblNewLabel_9());
+			registracijaPanel.add(getLblNewLabel_10());
+			registracijaPanel.add(getTxtIme());
+			registracijaPanel.add(getTxtPrezime());
+			registracijaPanel.add(getTxtEmail());
+			registracijaPanel.add(getTxtKorisnickoImeReg());
+			registracijaPanel.add(getTxtLozinkaReg());
+			registracijaPanel.add(getBtnNazadNaPrijavu());
+			registracijaPanel.add(getBtnRegistracija());
+		}
+		return registracijaPanel;
+	}
+	private JLabel getLblNewLabel_3() {
+		if (lblNewLabel_3 == null) {
+			lblNewLabel_3 = new JLabel("Nemate nalog?");
+			lblNewLabel_3.setFont(new Font("Times New Roman", Font.ITALIC, 15));
+			lblNewLabel_3.setBounds(19, 152, 130, 16);
+		}
+		return lblNewLabel_3;
+	}
+	private JButton getBtnRegistrujPanel() {
+		if (btnRegistrujPanel == null) {
+			btnRegistrujPanel = new JButton("Registrujte se!");
+			btnRegistrujPanel.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					((java.awt.CardLayout) contentPane.getLayout()).show(contentPane, "REGISTRACIJA");
+;
+				}
+			});
+			btnRegistrujPanel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			btnRegistrujPanel.setBounds(140, 147, 130, 29);
+		}
+		return btnRegistrujPanel;
+	}
+	private JLabel getLblNewLabel_4() {
+		if (lblNewLabel_4 == null) {
+			lblNewLabel_4 = new JLabel("Registracija");
+			lblNewLabel_4.setFont(new Font("Times New Roman", Font.BOLD, 15));
+			lblNewLabel_4.setBounds(50, 6, 108, 16);
+		}
+		return lblNewLabel_4;
+	}
+	private JLabel getLblNewLabel_5() {
+		if (lblNewLabel_5 == null) {
+			lblNewLabel_5 = new JLabel("Ime:");
+			lblNewLabel_5.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			lblNewLabel_5.setBounds(19, 34, 61, 16);
+		}
+		return lblNewLabel_5;
+	}
+	private JLabel getLblNewLabel_6() {
+		if (lblNewLabel_6 == null) {
+			lblNewLabel_6 = new JLabel("Prezime:");
+			lblNewLabel_6.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			lblNewLabel_6.setBounds(19, 62, 61, 16);
+		}
+		return lblNewLabel_6;
+	}
+	private JLabel getLblNewLabel_7() {
+		if (lblNewLabel_7 == null) {
+			lblNewLabel_7 = new JLabel("Email:");
+			lblNewLabel_7.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			lblNewLabel_7.setBounds(19, 90, 61, 16);
+		}
+		return lblNewLabel_7;
+	}
+	private JLabel getLblNewLabel_8() {
+		if (lblNewLabel_8 == null) {
+			lblNewLabel_8 = new JLabel("Korisnicko ime:");
+			lblNewLabel_8.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			lblNewLabel_8.setBounds(20, 118, 108, 16);
+		}
+		return lblNewLabel_8;
+	}
+	private JLabel getLblNewLabel_9() {
+		if (lblNewLabel_9 == null) {
+			lblNewLabel_9 = new JLabel("Lozinka:");
+			lblNewLabel_9.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			lblNewLabel_9.setBounds(19, 146, 61, 16);
+		}
+		return lblNewLabel_9;
+	}
+	private JLabel getLblNewLabel_10() {
+		if (lblNewLabel_10 == null) {
+			lblNewLabel_10 = new JLabel("Vec ste registrovani?");
+			lblNewLabel_10.setFont(new Font("Times New Roman", Font.ITALIC, 15));
+			lblNewLabel_10.setBounds(6, 240, 147, 16);
+		}
+		return lblNewLabel_10;
+	}
+	private JTextField getTxtIme() {
+		if (txtIme == null) {
+			txtIme = new JTextField();
+			txtIme.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			txtIme.setBounds(140, 34, 130, 26);
+			txtIme.setColumns(10);
+		}
+		return txtIme;
+	}
+	private JTextField getTxtPrezime() {
+		if (txtPrezime == null) {
+			txtPrezime = new JTextField();
+			txtPrezime.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			txtPrezime.setBounds(140, 57, 130, 26);
+			txtPrezime.setColumns(10);
+		}
+		return txtPrezime;
+	}
+	private JTextField getTxtEmail() {
+		if (txtEmail == null) {
+			txtEmail = new JTextField();
+			txtEmail.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			txtEmail.setBounds(140, 85, 130, 26);
+			txtEmail.setColumns(10);
+		}
+		return txtEmail;
+	}
+	private JTextField getTxtKorisnickoImeReg() {
+		if (txtKorisnickoImeReg == null) {
+			txtKorisnickoImeReg = new JTextField();
+			txtKorisnickoImeReg.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			txtKorisnickoImeReg.setBounds(140, 113, 130, 26);
+			txtKorisnickoImeReg.setColumns(10);
+		}
+		return txtKorisnickoImeReg;
+	}
+	private JPasswordField getTxtLozinkaReg() {
+		if (txtLozinkaReg == null) {
+			txtLozinkaReg = new JPasswordField();
+			txtLozinkaReg.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			txtLozinkaReg.setBounds(140, 141, 130, 26);
+		}
+		return txtLozinkaReg;
+	}
+	
+	private JButton getBtnNazadNaPrijavu() {
+		if (btnNazadNaPrijavu == null) {
+			btnNazadNaPrijavu = new JButton("Prijava");
+			btnNazadNaPrijavu.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					///// vrati na prijava panel
+					((java.awt.CardLayout) contentPane.getLayout()).show(contentPane, "PRIJAVA");
+				}
+			});
+			btnNazadNaPrijavu.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			btnNazadNaPrijavu.setBounds(140, 235, 130, 29);
+		}
+		return btnNazadNaPrijavu;
+	}
+/////// REGISTER|ime|prezime|username|email|password\n
+	private JButton getBtnRegistracija() {
+	    if (btnRegistracija == null) {
+	        btnRegistracija = new JButton("Registrujte se");
+	        btnRegistracija.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+
+	                String username = txtKorisnickoImeReg.getText().trim();
+	                String password = new String(txtLozinkaReg.getPassword()).trim();
+	                String ime = txtIme.getText().trim();
+	                String prezime = txtPrezime.getText().trim();
+	                String email = txtEmail.getText().trim();
+
+	                if (username.isEmpty() || password.isEmpty() || ime.isEmpty() || prezime.isEmpty() || email.isEmpty()) {
+	                    javax.swing.JOptionPane.showMessageDialog(null, "Sva polja moraju biti popunjena");
+	                    return;
+	                }
+
+	                if (!email.contains("@") || !email.contains(".")) {
+	                    javax.swing.JOptionPane.showMessageDialog(null, "Neispravan email");
+	                    return;
+	                }
+
+	                try {
+	                    Socket klijentSoket = new Socket("localhost", 8090);
+
+	                    DataOutputStream izlaz = new DataOutputStream(klijentSoket.getOutputStream());
+	                    BufferedReader ulaz = new BufferedReader(new InputStreamReader(klijentSoket.getInputStream()));
+
+	                    String poruka = "REGISTER|" + ime + "|" + prezime + "|" + username + "|" + email + "|" + password + "\n";
+	                    izlaz.writeBytes(poruka);
+
+	                    String odgovor = ulaz.readLine();
+	                    klijentSoket.close();
+
+	                    // OBRADA ODGOVORA
+	                    if ("OK|REGISTER".equals(odgovor)) {
+	                    	JOptionPane.showMessageDialog(null, "Uspešno ste se registrovali!");
+	                    	((java.awt.CardLayout) contentPane.getLayout()).show(contentPane, "PRIJAVA");
+	                    } else if (odgovor != null && odgovor.startsWith("ERR|")) {
+	                        javax.swing.JOptionPane.showMessageDialog(null, odgovor);
+	                    } else {
+	                        javax.swing.JOptionPane.showMessageDialog(
+	                                null, "Nepoznat odgovor servera: " + odgovor);
+	                    }
+
+	                } catch (Exception ex) {
+	                    javax.swing.JOptionPane.showMessageDialog(null, "Greška pri povezivanju sa serverom.");
+	                }
+	            }
+	        });
+
+	        btnRegistracija.setFont(new Font("Times New Roman", Font.BOLD, 15));
+	        btnRegistracija.setBounds(140, 179, 130, 29);
+	    }
+	    return btnRegistracija;
+	}
+
 }
